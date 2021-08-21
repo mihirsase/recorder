b3d94ee4e9968623478578026461a5d864f154e6
mihirsase <mihir@rirev.com>
2021-08-21 10:55:25 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Recorder());
+}
+
+class Recorder extends StatefulWidget {
+  const Recorder({Key? key}) : super(key: key);
+
+  @override
+  _RecorderState createState() => _RecorderState();
+}
+
+class _RecorderState extends State<Recorder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
